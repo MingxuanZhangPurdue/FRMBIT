d86f60033f7ddd69c9db7d5b3590136c53ac6163
MingxuanZhangPurdue <74074145+MingxuanZhangPurdue@users.noreply.github.com>
2024-05-03 21:35:18 -0400

    drop columns that are all nan

--- preprocessing.py
+++ preprocessing.py
@@ -13,12 +13,12 @@ with open(test_id_filename) as file:
     test_ids = [int(line.rstrip()) for line in file]
 test_ids = np.array(test_ids)
 
-heart = pd.read_csv("datasets/UKB_heart_bai82_Jan21_2021_QC_5mad_scale_resid_Sep19_2023.csv")
-abd = pd.read_csv("datasets/ukb_abdominal_idp_41_042923_all_5mad_scale_resid_Sep19_2023.csv")
-tfmri = pd.read_csv("datasets/tfMRI_2_all_5mad_scale_resid_Jun19_2023.csv")
-smri = pd.read_csv("datasets/sMRI_2_all_5mad_scale_resid_Jun19_2023.csv")
-rfmri = pd.read_csv("datasets/rfMRI_2_all_5mad_scale_resid_Jun19_2023.csv")
-dmri = pd.read_csv("datasets/dMRI_2_all_5mad_scale_resid_Jun19_2023.csv")
+heart = pd.read_csv("datasets/UKB_heart_bai82_Jan21_2021_QC_5mad_scale_resid_Sep19_2023.csv").dropna(axis=1, how='all')
+abd = pd.read_csv("datasets/ukb_abdominal_idp_41_042923_all_5mad_scale_resid_Sep19_2023.csv").dropna(axis=1, how='all')
+tfmri = pd.read_csv("datasets/tfMRI_2_all_5mad_scale_resid_Jun19_2023.csv").dropna(axis=1, how='all')
+smri = pd.read_csv("datasets/sMRI_2_all_5mad_scale_resid_Jun19_2023.csv").dropna(axis=1, how='all')
+rfmri = pd.read_csv("datasets/rfMRI_2_all_5mad_scale_resid_Jun19_2023.csv").dropna(axis=1, how='all')
+dmri = pd.read_csv("datasets/dMRI_2_all_5mad_scale_resid_Jun19_2023.csv").dropna(axis=1, how='all')
 
 dfs = [heart, abd, tfmri, smri, rfmri, dmri]
 merged_df = reduce(lambda left, right: pd.merge(left, right, on=['FID', 'IID'], how='outer'), dfs)
